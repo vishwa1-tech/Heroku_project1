ba4f24b4cb4aadc5da4cb3bb18b9657588a8e9ef
vishwajeet kumar <71384482+vishwa1-tech@users.noreply.github.com>
2022-04-20 01:37:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('xgb.pkl', 'rb'))
+
+
+
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+
+
+
+    return render_template('index.html', prediction_text='strokes prediction is  {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
